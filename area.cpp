14e05b692830986cf20d9501076b3a614f3a59ca
EvanDague <Evan.Dague@ucdenver.edu>
2016-05-08 22:07:07 -0600

    Update area.cpp

--- area.cpp
+++ area.cpp
@@ -1,12 +1,14 @@
 #include <cmath>
+#include <iostream>
+#include "Point.h"
 
 double computeArea(const Point &a, const Point &b, const Point &c){
     double length1, length2, length3, area;
     
-    length1 = distanceTo(a, b);
-    length2 = distanceTo(b, c);
-    length3 = distanceTo(c, a);
-    area = sqrt(4(pow(length1,2)*pow(length2,2)) - pow((pow(length1,2) + pow(length2,2) - pow(length3,2)),2))
+    length1 = b.distanceTo(a);
+    length2 = c.distanceTo(b);
+    length3 = a.distanceTo(c);
+    area = sqrt(4*(pow(length1,2)*pow(length2,2)) - pow((pow(length1,2) + pow(length2,2) - pow(length3,2)),2))/4;
     
     return area;
 }
